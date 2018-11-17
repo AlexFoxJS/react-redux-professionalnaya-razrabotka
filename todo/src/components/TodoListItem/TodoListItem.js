d1249920312b682b6ceffcb56cd0cefbf75f12e1
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-17 21:55:42 +0300

    master: 36 State  состояние React компонента
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- todo/src/components/TodoListItem/TodoListItem.js
+++ todo/src/components/TodoListItem/TodoListItem.js
@@ -3,23 +3,34 @@ import './TodoListItem.css'
 
 export default class TodoListItem extends Component {
 
+	state = {
+		done: false,
+	};
+
 	clickOnLabel = () => {
-		console.log(`Done: ${this.props.label}`)
+		this.setState({
+			done: true
+		})
 	};
 
 	render() {
 		const { label, important = false } = this.props;
+		const { done } = this.state;
 
 		const style = {
 			color: important ? 'steelblue' : 'black',
 			fontWeight: important ? 'bold' : 'normal',
 		};
 
+		let className = 'todoListItem';
+
+		if (done) className += ' done';
+
 		return (
-			<span className="todoListItem" style={style}>
+			<span className={className} style={style}>
 
 			<span
-				className="todo-list-item-label"
+				className="todoListItemLabel"
 				onClick={this.clickOnLabel}
 			>
 				{label}
